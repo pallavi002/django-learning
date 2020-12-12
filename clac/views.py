0a6640b782a9e37600818b4bc6a4c680d5cabdff
pallavi002 <pallavilodhi08@gmail.com>
2020-12-12 21:35:00 +0530

    some view added

--- clac/views.py
+++ clac/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+ 
+def home(request):
+    return render(request, "home.html", {
+        "name": "Yash",
+        "age":"18",
+        "height":"176 cm"
+    })
+
+
+
+
